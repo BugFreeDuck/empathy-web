43042e9c26ccfdb01486f44056e75466d5d956b2
g.kazakevicius <ga.kazakevicius@gmail.com>
2025-09-26 15:18:11 +0300

    Embed map fix

--- empathy-web/src/atomic/organisms/EmbededGoogleMap.svelte
+++ empathy-web/src/atomic/organisms/EmbededGoogleMap.svelte
@@ -1,10 +1,4 @@
-﻿<script lang="ts">
-    const apiKey: string = 'AIzaSyDkQH7KqFwZJJyi55sNxK-4lvIc-Ru7k2Q';
-    let embedQuery =`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Eitminų+g.+20,+Vilnius,+12118+Vilniaus+m.+sav.`
-
-</script>
-
-<style>
+﻿<style>
     iframe{
 
         height: 80%;
@@ -13,7 +7,8 @@
 
 <iframe
         title="map"
-        style="border:0"
-        referrerpolicy="no-referrer-when-downgrade"
-        src={embedQuery}>
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2303.3183747637104!2d25.215930313233716!3d54.739203569487714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd91af72194475%3A0x8c1502f3a4ec356f!2sempathy%20%7C%20space%20for%20movement%20%26%20growth!5e0!3m2!1sen!2sus!4v1758888931184!5m2!1sen!2sus"
+        style="border:0;"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
 </iframe>
